3ac19bf23825168598d88d77da5b94a2bdd7d366
Ankit Sharma <ankitsharma97194@gmail.com>
2023-02-10 14:45:16 +0530

    Change credential

--- src/main/java/com/spring/email/gmail/GmailSender.java
+++ src/main/java/com/spring/email/gmail/GmailSender.java
@@ -18,11 +18,11 @@ public class GmailSender {
 		Properties properties =new Properties();
 		properties.put("mail.smtp.auth",true);
 		properties.put("mail.smtp.starttls.enable",true);
-		properties.put("mail.smtp.port","587");
-		properties.put("mail.smtp.host","smtp.gmail.com");
+		properties.put("mail.smtp.port",""); //587
+		properties.put("mail.smtp.host",""); //"smtp.gmail.com
 		
-		final String username ="ankitsharma97194";
-		final String password="okqqfbycuteaavts"; //pass-code change
+		final String username ="";//ankitsharma97194
+		final String password=""; //pass-code change
 		
 		Session session = Session.getInstance(properties,new Authenticator() {
 
